d0e3555d5d36ee0144d274f7a3e8ec7ab7c354c6
samsuccess <samsuccess36@gmail.com>
2021-02-27 15:31:27 +0300

    Initial commit

---
+++ app/src/main/java/ru/example/findmyfilm/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.example.findmyfilm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.example.findmyfilm.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/example/findmyfilm/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.example.findmyfilm.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
